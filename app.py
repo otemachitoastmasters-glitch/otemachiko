3db7ea6063d2cf960f22205d4e7c78e1537ed3c1
otemachitoastmasters-glitch <otemachi.toastmasters@gmail.com>
2025-07-20 12:02:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,19 +6,16 @@ app = Flask(__name__)
 
 @app.route("/generate/")
 def generate_agenda():
-    mtgid = request.args.get("mtgid")
-    if not mtgid:
-        return "Missing mtgid parameter", 400
 
-    print(f"🔗 Fetching agenda from: {mtgid}")
-    output_path = generate_agenda_excel_from_url(mtgid, "meeting_agenda_template.xlsx")
+    print(f"🔗 Fetching agenda ")
+    output_path = generate_agenda_excel_from_url("meeting_agenda_template.xlsx")
     print(f"✅ Saved Excel to: {output_path}")
 
     # ✅ ダウンロードさせるレスポンス
     return send_file(output_path,
                      mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
                      as_attachment=True,
-                     download_name="Generated_Agenda.xlsx")
+                     download_name=output_path)
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=10000)
